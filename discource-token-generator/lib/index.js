91c32ad3ff78ad19bb424838ab4f1aec48775331
Kengo TODA <skypencil@gmail.com>
2020-12-15 05:56:10 +0800

    temporal commit

---
+++ discource-token-generator/lib/index.js
@@ -0,0 +1,64 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const os_1 = require("os");
+const open_1 = __importDefault(require("open"));
+const crypto_1 = require("crypto");
+const url_1 = __importDefault(require("url"));
+const { publicKey, privateKey } = crypto_1.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'top secret'
+    }
+});
+const server = http_1.createServer((req, res) => {
+    const queryObject = url_1.default.parse(req.url, true).query;
+    const encodedKey = queryObject.payload;
+    if (encodedKey === undefined) {
+        res.end();
+    }
+    else {
+        console.log(`encoded key is ${encodedKey}`);
+        //  privateDecrypt(privateKey, encodedKey)
+    }
+});
+function buildUrl(port, publicKey) {
+    const redirectUrl = `http://localhost:${port}/callback`;
+    const url = new URL('https://community.sonarsource.com//user-api-key/new');
+    url.searchParams.append('auth_redirect', redirectUrl);
+    url.searchParams.append('application_name', 'sonar-update-center-action');
+    url.searchParams.append('client_id', os_1.hostname());
+    url.searchParams.append('scopes', 'write');
+    url.searchParams.append('public_key', publicKey);
+    url.searchParams.append('nonce', '1');
+    return url.href;
+}
+server.listen(0, () => __awaiter(void 0, void 0, void 0, function* () {
+    const addressInfo = server.address();
+    if (addressInfo === null || typeof addressInfo === 'string') {
+        throw new Error(`Unexpected address info: ${addressInfo}`);
+    }
+    else {
+        const port = addressInfo.port;
+        yield open_1.default(buildUrl(port, publicKey));
+    }
+}));
