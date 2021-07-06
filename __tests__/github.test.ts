b3f7b77cfcd1a9856afa1a5aaa337752e33bc6c2
Kengo TODA <skypencil@gmail.com>
2021-07-06 11:06:16 +0800

    Make PRs created by Dependabot able to run integration/integrated test  (#173)
    * test: skip integration test with GitHub if GITHUB_TOKEN is absent
    
    * ci: run integration/integrated test with access to the GitHub Secrets
    
    https://securitylab.github.com/research/github-actions-preventing-pwn-requests/
    
    * ci: simplify the if in integration-test.yml
    
    * ci: rename the workflow file

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -1,15 +1,13 @@
 import {checkoutSourceRepo, fork} from '../src/github'
 
-const token = process.env.GITHUB_TOKEN
-if (!token) {
-  throw new Error('No GITHUB_TOKEN env var found')
-}
+const token = process.env.GITHUB_TOKEN || ''
+const integrationTest = token ? test : test.skip
 
-test('fork() does nothing if the forked repo already exists', async () => {
+integrationTest('fork() does nothing if the forked repo already exists', async () => {
   const resp = await fork(token)
 })
 
-test(
+integrationTest(
   'checkoutSourceRepo() can perform Git commands without error',
   async () => {
     const {owner} = await fork(token)
